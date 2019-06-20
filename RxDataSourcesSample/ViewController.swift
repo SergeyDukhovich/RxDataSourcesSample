c3c55b3a7e484d2b067f182ffee492f99e22a67b
Sergey Dukhovich <sergey.dukhovich@gmail.com>
2019-06-20 20:34:21 +0300

    Update table to RxDataSources implementation

--- RxDataSourcesSample/ViewController.swift
+++ RxDataSourcesSample/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import RxDataSources
 
 private enum CellIdentifier: String {
   case text
@@ -31,23 +32,46 @@ private extension Message {
 
 class ViewController: UIViewController {
 
-  private var messages = Observable<[Message]>.just([
-    .attributedText(NSAttributedString(string: "Blue text",
-                                       attributes: [NSAttributedString.Key.foregroundColor : UIColor.blue])),
-    .text("On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment"),
-    .photo(UIImage(named: "rx-wide")!),
-    .attributedText(NSAttributedString(string: "Red text",
-                                       attributes: [NSAttributedString.Key.foregroundColor : UIColor.red])),
-    .text("Another Message"),
-    .location(lat: 37.334722, lon: -122.008889),
-    .text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"),
-    .photo(UIImage(named: "rx-logo")!),
-    .attributedText(NSAttributedString(string: "Green text",
-                                       attributes: [NSAttributedString.Key.foregroundColor : UIColor.green])),
-    .location(lat: 53.9, lon: 27.56667),
-    .text("There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."),
-    .attributedText(NSAttributedString(string: "Yellow text",
-                                       attributes: [NSAttributedString.Key.foregroundColor : UIColor.yellow])),
+  let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, Message>>(configureCell: { dataSource, table, indexPath, message in
+    guard let cell = table.dequeueReusableCell(withIdentifier: message.identifier.rawValue) else { return UITableViewCell() }
+    switch message {
+    case let .text(message):
+      return ViewController.configure(text: message, cell: cell)
+    case let .attributedText(attributed):
+      return ViewController.configure(attributed: attributed, cell: cell)
+    case let .photo(photo):
+      return ViewController.configure(photo: photo, cell: cell)
+    case let .location(lat: lat, lon: lon):
+      return ViewController.configure(lat: lat, lon: lon, cell: cell)
+    }
+  })
+
+  private var messages = Observable<[SectionModel<String, Message>]>.just([
+    SectionModel<String, Message>(model: "1",
+                                  items: [
+                                    .attributedText(NSAttributedString(string: "Blue text",
+                                                                       attributes: [NSAttributedString.Key.foregroundColor : UIColor.blue])),
+                                    .text("On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment"),
+                                    .photo(UIImage(named: "rx-wide")!),
+                                    .attributedText(NSAttributedString(string: "Red text",
+                                                                       attributes: [NSAttributedString.Key.foregroundColor : UIColor.red]))
+      ]),
+    SectionModel<String, Message>(model: "2",
+                                  items: [
+                                    .text("Another Message"),
+                                    .location(lat: 37.334722, lon: -122.008889),
+                                    .text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"),
+                                    .photo(UIImage(named: "rx-logo")!)
+      ]),
+    SectionModel<String, Message>(model: "3",
+                                  items: [
+                                    .attributedText(NSAttributedString(string: "Green text",
+                                                                       attributes: [NSAttributedString.Key.foregroundColor : UIColor.green])),
+                                    .location(lat: 53.9, lon: 27.56667),
+                                    .text("There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."),
+                                    .attributedText(NSAttributedString(string: "Yellow text",
+                                                                       attributes: [NSAttributedString.Key.foregroundColor : UIColor.yellow]))
+      ])
     ])
 
   private let disposeBag = DisposeBag()
@@ -66,21 +90,13 @@ class ViewController: UIViewController {
 
     tableView.allowsSelection = false
 
+    dataSource.titleForHeaderInSection = { dataSource, index in
+      return dataSource.sectionModels[index].model
+    }
+
     messages
-      .bind(to: tableView.rx.items) { (table: UITableView, index: Int, message: Message) in
-        guard let cell = table.dequeueReusableCell(withIdentifier: message.identifier.rawValue) else { return UITableViewCell() }
-        switch message {
-        case let .text(message):
-          return ViewController.configure(text: message, cell: cell)
-        case let .attributedText(attributed):
-          return ViewController.configure(attributed: attributed, cell: cell)
-        case let .photo(photo):
-          return ViewController.configure(photo: photo, cell: cell)
-        case let .location(lat: lat, lon: lon):
-          return ViewController.configure(lat: lat, lon: lon, cell: cell)
-        }
-      }
-    .disposed(by: disposeBag)
+      .bind(to: tableView.rx.items(dataSource: dataSource))
+      .disposed(by: disposeBag)
   }
 
   static func configure(text: String, cell: UITableViewCell) -> UITableViewCell {
